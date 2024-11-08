a064dd9d1fac32c02e6881f479d871a200f478fd
Sindicat007 <sindicat007@ya.ru>
2024-11-08 09:53:40 +0500

    Calc update

--- src/Calc.java
+++ src/Calc.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите первое число: ");
         int firstVal = scanner.nextInt();
+
         System.out.println("Введите второе число: ");
         int secondVal = scanner.nextInt();
+
         System.out.println("Введите тип операции (+, -, *, /): ");
         char operation = scanner.next().charAt(0);
+
         calcValue(firstVal, secondVal, operation);
+
         scanner.close();
     }
     public static void calcValue(int a, int b, char c) {
