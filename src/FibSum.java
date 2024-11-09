9b64b71ea365920948edd8618d09cbcb4dd89ec1
Sindicat007 <sindicat007@ya.ru>
2024-11-09 20:41:43 +0500

    Форматирование
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

--- src/FibSum.java
+++ src/FibSum.java
@@ -2,11 +2,12 @@ public class FibSum {
     public static void main(String[] args) {
         System.out.println(fibRec(6));
     }
-    public static int fibFor (int a) {
+
+    public static int fibFor(int a) {
         int result = 0;
         int buf1 = 0;
         int buf2 = 1;
-        for (int i = 1; i < a; i++){
+        for (int i = 1; i < a; i++) {
             result = buf1 + buf2;
             buf1 = buf2;
             buf2 = result;
@@ -14,7 +15,7 @@ public class FibSum {
         return result;
     }
 
-    public static int fibRec (int a) {
+    public static int fibRec(int a) {
         if (a <= 1) return a;
         return fibRec(a - 1) + fibRec(a - 2);
     }
