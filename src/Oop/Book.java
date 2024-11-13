549f9deb598d98e78d370c531c71dad0d2ed266a
Sindicat007 <sindicat007@ya.ru>
2024-11-13 19:40:48 +0500

    Задания по ООП
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

---
+++ src/Oop/Book.java
@@ -0,0 +1,36 @@
+package Oop;
+
+public class Book {
+    private String title;
+    private int year;
+    private Author author;
+
+    public Book(String title, int year, Author author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: '" + title + '\'' +
+                ", Год публикации: " + year +
+                "\nАвтор: " + author + "\n";
+    }
+}
