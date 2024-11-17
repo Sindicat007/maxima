6d130f59ce35e7c85cf9bc5ce3696ab5b3c41ba4
Sindicat007 <sindicat007@ya.ru>
2024-11-17 11:25:16 +0500

    Редактирование структуры
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

--- src/Cycle/Calc.java
+++ src/Cycle/Calc.java
@@ -1,3 +1,5 @@
+package Cycle;
+
 import java.util.Scanner;
 
 public class Calc {
--- src/Git/Main.java
+++ src/Git/Main.java
@@ -1,3 +1,5 @@
+package Git;
+
 class Main {
     public static void main(String[]args) {
         System.out.println("Test task Git");
