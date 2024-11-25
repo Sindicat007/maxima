188916f64c8355ec715dd52b29a226b058a53be7
Sindicat007 <sindicat007@ya.ru>
2024-11-25 23:45:41 +0500

    Задачи с LeedCode
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

--- src/leetcode/Palindrome.java
+++ src/leetcode/Palindrome.java
@@ -1,23 +1,11 @@
 package leetcode;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Palindrome {
 
     public static void main(String[] args) {
+
         int a = 12221;
         System.out.println(isPalindrome(a));
-
-
-        char[] sym = String.valueOf(a).toCharArray();
-        StringBuilder strb = new StringBuilder();
-        strb.append(sym);
-        System.out.println(strb.reverse());
-//        String b = new StringBuilder(sym.toString());
-        System.out.println(String.valueOf(sym));
-//        System.out.println(isPalindrome(121));
-
     }
 
     public static boolean isPalindrome(int x) {
