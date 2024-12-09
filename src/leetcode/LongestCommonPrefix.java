3a48714afacd35cb38fc993514d6df40588fdfc7
Sindicat007 <sindicat007@ya.ru>
2024-12-09 23:29:29 +0500

    LeetCode Prefix Task
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -2,16 +2,22 @@ package leetcode;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
     }
 
-    public String longestCommonPrefix(String[] str) {
-        if (str == null || str.length == 0) {
+    public static String longestCommonPrefix(String[] str) {
+        if (str == null || str.length < 1) {
             return "";
         }
-        for (int i = 0; i < str.length; i++){
-            return str[i];
+        String prefix = str[0];
+        for (int i = 1; i < str.length; i++) {
+            while (str[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
         }
-        return "";
+        return prefix;
     }
 }
