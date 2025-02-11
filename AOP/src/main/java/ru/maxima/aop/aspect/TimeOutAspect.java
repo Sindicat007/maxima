c264080d328c086bdd661c47ae0ebea0fe539bd0
Sindicat007 <sindicat007@ya.ru>
2025-02-11 19:06:30 +0500

    Добавил инициализацию меременной lastExecutionTime, Заменил задержку потока на выход из метода, настроил правильный подсчет времени
    Signed-off-by: Sindicat007 <sindicat007@ya.ru>

--- AOP/src/main/java/ru/maxima/aop/aspect/TimeOutAspect.java
+++ AOP/src/main/java/ru/maxima/aop/aspect/TimeOutAspect.java
@@ -9,20 +9,20 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class TimeOutAspect {
 
-    private static long time;
+    private static long lastExecutionTime = 0;
 
     @Around("execution(* ru.maxima.aop.service.MyService.doWithTimeOut(..))")
-    public Object timeoutExecution(ProceedingJoinPoint joinPoint) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        if (time == 0) {
-            time = System.currentTimeMillis();
-        } else if (time - startTime < 5000) {
-            System.out.println("Процесс продолжится через  " + (5000 - (time - startTime)) + "\n");
-            Thread.sleep(5000 - (time - startTime));
+    public void timeoutExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (lastExecutionTime == 0) {
+            lastExecutionTime = System.currentTimeMillis();
+        } else if (System.currentTimeMillis() - lastExecutionTime < 5000) {
+            System.out.println("Повторно выполнить метод можно будет через " +
+                    (5000 - (System.currentTimeMillis() - lastExecutionTime)) +
+                    " миллисекунд\n");
+            return;
         }
         joinPoint.proceed();
         System.out.println();
-        time = System.currentTimeMillis();
-        return "";
+        lastExecutionTime = System.currentTimeMillis();
     }
 }
